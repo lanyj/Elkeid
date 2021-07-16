814bc1f8483d32ef7983ab6c3dec7f8ccef96c7c
Jay Lan <403678693@qq.com>
2021-07-16 22:18:32 +0800

    Update SmithProbe.java
    在retransform的时候，如果一个class失败，会导致全部被拒绝，所以需要单个进行操作

--- rasp/jvm/JVMProbe/src/main/java/com/security/smith/SmithProbe.java
+++ rasp/jvm/JVMProbe/src/main/java/com/security/smith/SmithProbe.java
@@ -79,23 +79,15 @@ public class SmithProbe implements ClassFileTransformer, ProbeNotify {
     }
 
     private void reloadClasses(Collection<String> classes) {
-        List<Class<?>> cls = new ArrayList<>();
-
         for (String className : classes) {
             try {
-                Class<?> cl = Class.forName(className, true, ClassLoader.getSystemClassLoader());
-                cls.add(cl);
+                inst.retransformClasses(Class.forName(className, true, ClassLoader.getSystemClassLoader()));
+                SmithLogger.logger.info("reload: " + cls);
             } catch (ClassNotFoundException e) {
                 SmithLogger.logger.info("class not found: " + className);
+            } catch (UnmodifiableClassException e) {
+                SmithLogger.exception(e);
             }
-        }
-
-        SmithLogger.logger.info("reload: " + cls);
-
-        try {
-            inst.retransformClasses(cls.toArray(new Class<?>[0]));
-        } catch (UnmodifiableClassException e) {
-            SmithLogger.exception(e);
         }
     }
 
